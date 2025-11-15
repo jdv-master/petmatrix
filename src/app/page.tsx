7529c026b66a1ff5689b54d3ba9a95100f4a5723
jdv-master <daniel@allmatrix.com.br>
2025-11-14 21:42:27 -0300

    Projeto Inicial PetShop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-    return (
-      <div>
-        <h2>Bem Vindo</h2>
-        <h1>All Matrix</h1>
-      </div>
-    );
+  return (
+    <div>
+      <h2>Bem Vindo</h2>
+    </div>
+  );
 }
